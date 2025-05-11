612bc27ceec1a131561d697507928dc44b5f8401
Vesper Solutions <78233861+vespersolutionspk18@users.noreply.github.com>
2025-05-11 17:03:48 +0500

    Nexus

--- app/home/FutureNowSection.tsx
+++ app/home/FutureNowSection.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import Spline from '@splinetool/react-spline/next'
+// import Spline from '@splinetool/react-spline/next' // Commenting out as it's no longer used
 import FutureMarquee from './FutureMarquee'
 
 const FutureNowSection = () => {
   return (
     <div className="w-full overflow-hidden">
-      <div className=" relative h-[620px] md:h-screen overflow-hidden" id="spline here">
-        <Spline
-          className="absolute inset-0 w-full h-full sm:scale-100 sm:translate-x-0"
-          style={{ transform: 'scale(1.4) ' }}
-          scene="https://prod.spline.design/ubI5i3wBPrOoZjHc/scene.splinecode"
+      <div className=" relative h-[620px] md:h-screen overflow-hidden flex items-center justify-center" id="video-container">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute inset-0 w-full h-full object-cover"
+          style={{ transform: 'scale(1.35)' }}
+          src="/assets/particles.mp4"
         />
         <div className="absolute inset-0 z-10 flex items-center justify-center">
           <FutureMarquee />
